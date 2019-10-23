009300d2bad21ddcd216d5f2544c4a216e36ab8c
Hortense <hortense@pelay.fr>
2019-10-23 14:25:14 +0200

    end of challenge

--- routes/index.js
+++ routes/index.js
@@ -9,13 +9,14 @@ var dataBike = [
     { name: "GEWO8", url: "/images/bike-4.jpg", price: 1249 },
     { name: "KIWIT", url: "/images/bike-5.jpg", price: 899 },
     { name: "NASAY", url: "/images/bike-6.jpg", price: 1399 }
-]
+];
 
 // Bikes array that we will use in shop.ejs to desplay our bikes list to buy
-var dataCardBike = [
-    { name: "BIKO45", url: "/images/bike-1.jpg", price: 679, quantity: 1 },
-    { name: "ZOOK7", url: "/images/bike-2.jpg", price: 799, quantity: 2 },
-]
+var dataCardBike = [];
+
+function deleteBike(pos) {
+    dataCardBike.splice(pos, 1);
+}
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
@@ -25,7 +26,33 @@ router.get('/', function(req, res, next) {
 /* GET Shop page. */
 router.get('/shop', function(req, res, next) {
     res.render('shop', { dataCardBike });
-    console.log(req.query);
+});
+
+/* POST Shop page. */
+router.post('/shop', function(req, res, next) {
+    dataCardBike.push({
+        name: req.body.bikeNameFromFront,
+        url: req.body.bikeImageFromFront,
+        price: req.body.bikePriceFromFront,
+        quantity: req.body.bikeQuantityFromFront
+    })
+
+    console.log("/shop", req.body);
+    res.render('shop', { dataCardBike });
+});
+
+/* POST delete-Shop page. */
+router.post('/delete-shop', function(req, res, next) {
+    deleteBike(req.body.position);
+    res.render('shop', { dataCardBike });
+});
+
+/* POST update-Shop page. */
+router.post('/update-shop', function(req, res, next) {
+    var position = req.body.position;
+    var newQuantity = req.body.quantity;
+    dataCardBike[position].quantity = newQuantity;
+    res.render('shop', { dataCardBike });
 });
 
 module.exports = router;
